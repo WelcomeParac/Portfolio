e163c026f5fcb389711ff7f9404c0d486ccc6339
WelcomeParac <Aaronhardyofficial@gmail.com>
2023-07-08 16:50:29 -0700

    Implemented Dark Mode Toggle
    Updated the JavaScript file to introduce a function for toggling the dark mode on and off.
    Implemented logic to save the dark mode state on website reload using local storage.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+const theme = localStorage.getItem('theme');
+
+
+theme && document.body.classList.add(theme);
+
+function darkMode(){
+
+  let element = document.body; {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+      localStorage.setItem('theme', 'dark-mode');
+    } else {
+      localStorage.removeItem('theme');
+    }
+  };
+  
+  
+}
