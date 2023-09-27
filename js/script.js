d11b9111f549749fbc1ccb6bd89637ae39797709
WelcomeParac <Aaronhardyofficial@gmail.com>
2023-09-27 09:22:24 -0700

    added images and fixed dark mode

--- js/script.js
+++ js/script.js
@@ -9,16 +9,27 @@ const theme = localStorage.getItem('theme');
 
 theme && document.body.classList.add(theme);
 
-function darkMode(){
+// Function to toggle dark mode
+function darkMode() {
+  const element = document.body;
 
-  let element = document.body; {
-    document.body.classList.toggle('dark-mode');
-    if (document.body.classList.contains('dark-mode')) {
-      localStorage.setItem('theme', 'dark-mode');
-    } else {
-      localStorage.removeItem('theme');
-    }
-  };
-  
-  
+  // Toggle the 'dark-mode' class on the body
+  element.classList.toggle('dark-mode');
+
+  if (element.classList.contains('dark-mode')) {
+    localStorage.setItem('theme', 'dark-mode');
+    // Switch to the dark mode image
+    document.querySelector('.portfolio-header-image').src = './assets/images/HornTorus(1).png';
+  } else {
+    localStorage.removeItem('theme');
+    // Switch to the light mode image
+    document.querySelector('.portfolio-header-image').src = './assets/images/Horntorusblack1.png';
+  }
+}
+
+// Set the initial image based on the theme
+if (theme === 'dark-mode') {
+  document.querySelector('.portfolio-header-image').src = './assets/images/HornTorus(1).png';
+} else {
+  document.querySelector('.portfolio-header-image').src = './assets/images/Horntorusblack1.png';
 }
